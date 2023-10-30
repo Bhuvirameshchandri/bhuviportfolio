139022510dda22db57331aca52b657b98a229e3f
Bhuvirameshchandri <149400649+Bhuvirameshchandri@users.noreply.github.com>
2023-10-30 20:18:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+
+
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var message = document.getElementById("message").value;
+
+    if (name.trim() === "" || email.trim() === "" || message.trim() === "") {
+        alert("Please fill in all the fields.");
+    } else {
+        alert("Form submitted successfully!");
+        document.getElementById("contactForm").reset();
+    }
+});
